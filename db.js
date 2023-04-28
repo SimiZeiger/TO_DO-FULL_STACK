8a2d445f73e4d5a8b81568b2e1486d1869ff63dd
Simi <szeiger03@gmail.com>
2023-04-27 21:57:15 -0400

    moved package.json to root directory and added commands to script

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 require("dotenv").config();
 
 const pool = new Pool({
-    connectionString: process.env.db_localhost,
+    connectionString: process.env.db_host,
     ssl: {
         rejectUnauthorized: false
     }
