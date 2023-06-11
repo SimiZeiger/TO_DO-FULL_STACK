7d7670241f67aea581e4de43b1efc2d55a7179df
Simi <szeiger03@gmail.com>
2023-06-11 11:41:50 -0400

    edited code in db.js (connecting to postgres

--- db.js
+++ db.js
@@ -3,8 +3,7 @@ const Pool = require("pg").Pool;
 require("dotenv").config();
 
 const pool = new Pool({
-    connectionString: process.env.db_localhost,
-
+  connectionString: process.env.db_localhost,
 });
 
 module.exports = pool;
